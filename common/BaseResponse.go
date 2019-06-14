d8551859d9e6d34d49f2d4aa440a900e897c2c0f
git <fulunyong@126.com>
2019-06-14 16:59:51 +0800

    公共返回包

--- common/BaseResponse.go
+++ common/BaseResponse.go
@@ -1,7 +1,19 @@
 package common
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type BaseResponse struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
 	Data interface{} `json:"data"`
+}
+
+//统一返回处理
+func Render(w http.ResponseWriter, response BaseResponse) {
+	w.WriteHeader(http.StatusOK)
+	bytes, _ := json.Marshal(response)
+	_, _ = w.Write(bytes)
 }
